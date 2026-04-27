2836be87c8e424a694163ddb61090fbfd4e98ad0
Mario Korzun <rkorzun2020@gmail.com>
2026-04-27 16:29:38 +0400

    feat: initialize vanilla app template with Vite, CSS styles, and basic HTML structure
    - Added package.json for project configuration and dependencies
    - Created common CSS styles for layout and typography
    - Developed weather-specific CSS styles for search form and weather card
    - Set up index.html with a search form and weather display area
    - Implemented initial JavaScript file with TODOs for API integration and weather card template
    - Configured Vite with plugins for HTML injection, full reload, and CSS sorting

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+//TODO: API (https://openweathermap.org/api/current?collection=current_forecast)
+
+/*
+TODO: Weather card template:
+
+<div class="weather-card">
+  <h2 class="city-name"></h2>
+  <ul class="weather-info list">
+    <li class="weather-info-item">
+        <p class="temp">Температура: <sup>&#176;</sup></p>
+    </li>
+    <li class="weather-info-item">
+        <p class="feels-like-temp">Відчувається як: <sup>&#176;</sup></p>
+    </li>
+    <li class="weather-info-item">
+        <p class="sunrise-time">Схід сонця: </p>
+    </li>
+    <li class="weather-info-item">
+        <p class="sunset-time">Захід сонця: </p>
+    </li>
+    <li class="weather-info-item">
+        <p class="clouds">Хмарність: %</p>
+    </li>
+  </ul>
+</div>
+*/
